32d245452d2fd9909567aaff58eb14e68c4bb3ad
Shalinirai498 <shalinirai0672@gmail.com>
2024-05-13 13:24:20 +0530

    Your commit message here

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>| Online Quiz System |</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+    <style type="text/css">
+        body {
+            width: 100%;
+            background: url(image/book.png) ;
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+        .navbar {
+            background-color: #333;
+            overflow: hidden;
+        }
+        .navbar a {
+            float: right;
+            display: block;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+        .intro {
+            margin-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="admin.php" class="btn">Admin</a>
+    </div>
+    <center>
+        <div class="intro">
+            <h1>Quizzo-Online Quiz System</h1>
+            <a href="login.php" class="btn">Login</a>&emsp;
+            <a href="register.php" class="btn">Register</a>
+            <h2>Good Luck.</h2>
+        </div>
+    </center>
+</body>
+</html>
